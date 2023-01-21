cd66f6d91f8243b42d70562e41226d6db1711a65
robvergara <robertver0306@gmail.com>
2023-01-20 20:59:13 -0500

    agregar boton cancelar en ventana logout

--- src/LogOutPage/index.js
+++ src/LogOutPage/index.js
@@ -1,23 +1,34 @@
 import React from "react";
+import { useNavigate } from "react-router";
 import { useAuth } from "../contexts/auth";
 
 export function LogOutPage(){
   const auth = useAuth();
+  const navigate = useNavigate()
 
   const logout = (e) =>{
     e.preventDefault();
     auth.logout()
   }
 
+  const onCancel= (e)=>{
+    e.preventDefault();
+    navigate(-1)
+  }
+
   return(
     <>
       <h1>Logout</h1>
 
-      <form onSubmit={logout}>
+      <form className="container text-center" onSubmit={logout}>
+        <div className="row mb-3">
+          <label className="col">Seguro que quieres salir?</label>
+        </div>
 
-        <label >Seguro que quieres salir?</label>
-
-        <button className="btn btn-primary col-sm-2 mx-auto" type="submit">Salir</button>
+        <div className="btn-group">
+          <button className=" btn btn-primary " type="submit">Salir</button>
+          <button className=" btn btn-primary " onClick={onCancel}>cancelar</button>
+        </div>
       </form>
     </>
   )
