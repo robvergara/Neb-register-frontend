2167c90b1e1ac15d4656ef888db65c1486b7888b
Pablo Dos Santos <pablo.dossantos@ibisa.co>
2023-02-23 08:56:30 -0500

    cambios footer

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -2,8 +2,23 @@ import React from "react";
 
 export function Footer(){
   return(
-    <>
-      <h1>Footer</h1>
-    </>
+    // <>
+    //   <h1>Footer</h1>
+    // </>
+    <div className="footer mt-auto py-2 bg-light">
+      <div className="container">
+        <div className="row">
+          <div className="col-3 text-center">
+            <img src="neb-logo.png" className="w-50 my-4" alt="Logo"/>
+          </div>
+          <div className="col-6">
+            <img src="neb-logo.png" className="w-25 my-4" alt="Logo"/>
+          </div>
+          <div className="col-3">
+            <img src="neb-logo.png" className="w-25 my-4" alt="Logo"/>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
