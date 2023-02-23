cae9df46a95731ec6b9cd54065c8988d4b83a3d8
Pablo Dos Santos <pablo.dossantos@ibisa.co>
2023-02-23 09:06:16 -0500

    modificaciones footer

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -12,7 +12,9 @@ export function Footer(){
             <img src="neb-logo.png" className="w-50 my-4" alt="Logo"/>
           </div>
           <div className="col-6">
-            <img src="neb-logo.png" className="w-25 my-4" alt="Logo"/>
+            <h2>NEB COLOMBIA</h2>
+            <h5>Profesor: Deivis José Seidel Delgado</h5>
+            <h6>Cel: +57 301 3679934</h6>
           </div>
           <div className="col-3">
             <img src="neb-logo.png" className="w-25 my-4" alt="Logo"/>
