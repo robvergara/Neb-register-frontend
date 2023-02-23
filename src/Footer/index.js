90ceb66277da703f405d7eda02136e0ca07a6b6d
robvergara <robertver0306@gmail.com>
2023-02-23 07:51:26 -0500

    estilando footer de la app

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,9 +1,24 @@
 import React from "react";
+import logo from "../assets/images/neb-logo.png"
+import "./Footer.css"
 
 export function Footer(){
   return(
     <>
-      <h1>Footer</h1>
+      <footer className="section seccion-oscura d-flex flex-column justify-content-center align-items-center mt-3">
+        <img className="footer-logo" src={logo} alt="logo de la pagina"/>
+        {/* <p className="footer-texto text-center"> NEB Colombia. </p> */}
+        <div className="d-flex flex-wrap align-items-center justify-content-center">
+          <a className="iconos-redes-sociales " href="https://instagram.com/escuelanebcolombia?igshid=YmMyMTA2M2Y=" target="_blank" rel="noopener noreferrer">
+            <i className="bi bi-instagram"></i>
+          </a>
+          <a className="iconos-redes-sociales " href="mailto:janedoe@micorreo.com">
+              <i className="bi bi-envelope-at-fill"></i>
+          </a>
+        </div>
+
+        <div className="derechos-de-autor">Creado por Robert Vergara (2023) &#169;</div>
+      </footer>
     </>
   )
 }
