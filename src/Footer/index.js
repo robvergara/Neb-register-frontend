4fd54d89bd2eb851e1ac24089011691db68a33a9
robvergara <robertver0306@gmail.com>
2023-02-23 19:42:09 -0500

    cambios para hacer merge con el footer de pablo

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -5,25 +5,6 @@ import logo from "../assets/images/neb-logo.png"
 
 export function Footer(){
   return(
-    <>
-      <div className="footer mt-auto py-2 bg-light">
-        <div className="container">
-          <div className="row">
-            <div className="col-3 text-center">
-              <img src="neb-logo.png" className="w-50 my-4" alt="Logo"/>
-            </div>
-            <div className="col-6">
-              <h2>NEB COLOMBIA</h2>
-              <h5>Profesor: Deivis José Seidel Delgado</h5>
-              <h6>Cel: +57 301 3679934</h6>
-            </div>
-            <div className="col-3">
-              <img src="neb-logo.png" className="w-25 my-4" alt="Logo"/>
-            </div>
-          </div>
-        </div>
-      </div>
-      
       <>
         <footer className="section seccion-oscura d-flex flex-column justify-content-center align-items-center mt-3">
           <img className="footer-logo" src={logo} alt="logo de la pagina"/>
@@ -40,7 +21,5 @@ export function Footer(){
           <div className="derechos-de-autor">Creado por Robert Vergara (2023) &#169;</div>
         </footer>
       </>
-
-    </>
   )
 }
