28275317826ab0e80c06b80d57582296aba365d2
Pablo Dos Santos <pablo.dossantos@ibisa.co>
2023-02-24 08:41:24 -0500

    index homepage

--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -4,7 +4,7 @@ import { PaymentsList } from "./PaymentsList";
 
 export function HomePage(){
   return(
-    <div className="p-4">
+    <div className="p-4 px-5">
       <HomeSearch/>
       <PaymentsList/>
     </div>
