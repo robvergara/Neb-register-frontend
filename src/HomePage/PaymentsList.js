78fd690f3007cf5c861d339905e1b77945917e88
Pablo Dos Santos <pablo.dossantos@ibisa.co>
2023-02-24 09:12:10 -0500

    tabla de pagos

--- src/HomePage/PaymentsList.js
+++ src/HomePage/PaymentsList.js
@@ -23,25 +23,17 @@ export function PaymentsList(){
 
   return(
     <>
-      {/* {payments.length <= 0  && (
-        <div className="container text-center">
-          <h2 className="my-5"><b>Realiza tu busqueda</b></h2>
-        </div>
-      )} */}
-
       {payments.length >= 1 && (
-        <div className="container m-auto">
-          <table className="table table-striped caption-top">
+        <div className="container-sm m-auto bg-light rounded py-3 px-5">
+          <table class="table table-striped caption-top mb-2">
             <caption>
                 <div className="d-flex justify-content-between">
                   Pagos realizados por {estudianteQuePago.nombre} {estudianteQuePago.apellido}
-                  {auth.user?.status === 1 && 
                   <div className="col-sm-3 btn-group">
                     <NavLink className="btn btn-primary" to={"/pagos"} onClick={()=>setdataPayment(estudianteQuePago)}> 
                       Realizar Pago 
                     </NavLink>  
                   </div>
-                  }
                 </div>
             </caption>
     
@@ -62,27 +54,7 @@ export function PaymentsList(){
               ))}
             </tbody>
           </table>
-          {/* <div className="col-sm-10 text-center">
-            <div className="mb-3 row">
-              <h4 className="col-sm-9">Pagos realizados por {estudianteQuePago.nombre} {estudianteQuePago.apellido}</h4>
-              {auth.user?.status === 1 && 
-                <div className="col-sm-3 btn-group">
-                  <NavLink className="btn btn-primary" to={"/pagos"} onClick={()=>setdataPayment(estudianteQuePago)}> 
-                    nuevo pago 
-                  </NavLink>  
-                </div>
-              }
-            </div>
-          </div> */}
-          {/* <div>
-            {payments.map(payment=> (
-              <div key={payment._id}>
-                <p><b>Fecha de pago: </b> {payment.mes} de {payment.ano}</p>
-              </div>
-            ))}
-          </div> */}
         </div>
-        
       )}
     </>
   );
