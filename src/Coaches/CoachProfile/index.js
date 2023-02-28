cd1bad0a52e9c01a8d9dd3f1bc46c06e5bcb1a2d
Pablo Dos Santos <pablo.dossantos@ibisa.co>
2023-02-28 11:09:31 -0500

    entrenadores

--- src/Coaches/CoachProfile/index.js
+++ src/Coaches/CoachProfile/index.js
@@ -24,18 +24,15 @@ export function CoachProfile(){
         <div className="card p-5">
           <div className="row">
             <div className="col-md-2 d-flex justify-content-center my-2">
-              <img src="sin-foto.png" className="img-thumbnail"/>
+              <img src="sin-foto.png" className="img-thumbnail imagen"/>
             </div>
             <div className="col-md-10 my-2">
-              <h1>Perfil de {coach.nombre} {coach.apellido} </h1>
+              <h1>{coach.nombre} {coach.apellido} </h1>
               <p><b> Cedula: </b> {coach.cedula}</p>
             </div>
           </div>
-          
-          
         </div>
       </div>
-      
     </>
   )
 }
